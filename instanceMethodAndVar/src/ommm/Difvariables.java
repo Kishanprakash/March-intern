e691dd9679eda1e22fc28e190dc29be817bb9c55
Kishanprakash <109814608+Kishanprakash@users.noreply.github.com>
2025-03-24 11:48:07 +0530

    instance operations

---
+++ instanceMethodAndVar/src/ommm/Difvariables.java
@@ -0,0 +1,17 @@
+package ommm;
+
+public class Difvariables {
+    public String name ;
+    String compamy ;
+    private char grade ;
+
+    private   void dispaly(){
+        System.out.println("young");
+    }
+    public static void stream(){
+        System.out.println("water");
+    }
+    void reuse(){
+        System.out.println('d');
+    }
+}
---
+++ instanceMethodAndVar/src/ommm/Mainrunner.java
@@ -0,0 +1,10 @@
+package ommm;
+
+public class Mainrunner {
+    public static void main(String[] args) {
+        Difvariables dif =new Difvariables();
+
+        Difvariables.stream();
+        dif.reuse();
+    }
+}
