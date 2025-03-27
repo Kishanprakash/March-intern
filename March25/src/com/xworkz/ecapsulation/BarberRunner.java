97d146bbff9b77584049a227ecb66e9fe2059465
Kishanprakash <109814608+Kishanprakash@users.noreply.github.com>
2025-03-27 08:54:21 +0530

    adding

---
+++ March25/src/com/xworkz/ecapsulation/BarberRunner.java
@@ -0,0 +1,15 @@
+package com.xworkz.ecapsulation;
+
+public class BarberRunner {
+
+    public static void main(String[] args) {
+        Barbershop barbershop = new Barbershop();
+        System.out.println(barbershop.name);
+        Barbershop b=new Barbershop();
+        b.name="Kantharaj";
+        Barbershop b1=new Barbershop();
+        b1.name="Varun";
+        System.out.println(b.name);
+        System.out.println(b1.name);
+    }
+}
---
+++ March25/src/com/xworkz/ecapsulation/Barbershop.java
@@ -0,0 +1,9 @@
+package com.xworkz.ecapsulation;
+
+public class Barbershop {
+    public static String name;
+    static{
+        name="halageri";
+        System.out.println("Running static block");
+    }
+}
